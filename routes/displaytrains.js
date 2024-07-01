3d004ac628d2ef3e9b0130e0db3cfc97e1c1f33c
Somil Gupta <gsomil805@gmail.com>
2024-07-01 13:45:46 +0530

    single page loader complete

--- routes/displaytrains.js
+++ routes/displaytrains.js
@@ -6,11 +6,30 @@ const router = express.Router();
 router.get('/', async(req,res)=>{
     try {
         const { fromLocation, toLocation, departureDate, returnDate, classChoice } = req.query;
-
-        const trainData = await TRAIN.findOne({ From: fromLocation, To: toLocation, DepartDate: new Date(departureDate), ReturnDate: new Date(returnDate), Class:classChoice });
         // Initialize comparison result
         let comparisonResult = {};
         let data = [];
+
+        if(fromLocation && toLocation && departureDate==='' && returnDate==='' && classChoice==='') {
+            const trainData = await TRAIN.find({ From: fromLocation, To: toLocation });
+            if (trainData) {
+                trainData.forEach((train) => {
+                    const comparisonResult = {
+                        from: train.From === fromLocation,
+                        to: train.To === toLocation
+                    };
+    
+                    if (comparisonResult.from && comparisonResult.to) {
+                        data.push(train);
+                    }
+                });
+            } 
+           else {
+                comparisonResult.error = 'Flight not found';
+            } 
+            res.render('../views/displaytrains.ejs', { data});
+        }
+        const trainData = await TRAIN.findOne({ From: fromLocation, To: toLocation, DepartDate: new Date(departureDate), ReturnDate: new Date(returnDate), Class:classChoice });
         
         // Compare the data
         if (trainData) {
@@ -25,8 +44,6 @@ router.get('/', async(req,res)=>{
         } else {
             comparisonResult.error = 'train not found';
         }
-        
-        console.log(comparisonResult);
         res.render('../views/displaytrains.ejs', {data});
     }catch(err){
         res.status(500)
