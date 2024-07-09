4673df8f423aa313876ba51c29c1966e903db5cf
Somil Gupta <gsomil805@gmail.com>
2024-07-09 16:11:42 +0530

    display page sorted

--- routes/displayflights.js
+++ routes/displayflights.js
@@ -1,54 +1,54 @@
 import express from "express";
-import Flight from "../models/flight.js"
+import Flight from "../models/flight.js";
 
 const router = express.Router();
 
-router.get('/', async(req,res)=>{
+router.get('/', async (req, res) => {
     try {
-        const { fromLocation, toLocation, departureDate, returnDate, classChoice } = req.query;
-        // Initialize comparison result
-        let comparisonResult = {};
+        const { fromLocation, toLocation, departureDate,returnDate, classChoice } = req.query;
         let data = [];
-        
-        if(fromLocation && toLocation && departureDate==='' && returnDate==='' && classChoice==='') {
-            const flightData = await Flight.find({ From: fromLocation, To: toLocation });
-            if (flightData) {
-                flightData.forEach((flight) => {
-                    const comparisonResult = {
-                        from: flight.From === fromLocation,
-                        to: flight.To === toLocation
-                    };
-    
-                    if (comparisonResult.from && comparisonResult.to) {
-                        data.push(flight);
-                    }
-                });
-            } 
-           else {
-                comparisonResult.error = 'Flight not found';
-            }
-            res.render('../views/displayflights.ejs', { data});
-        }
-        const flightData = await Flight.findOne({ From: fromLocation, To: toLocation, DepartDate: new Date(departureDate), ReturnDate: new Date(returnDate), Class:classChoice });
 
+        if (fromLocation && toLocation && departureDate === '' && returnDate === '' && classChoice === '') {
+            const flightData = await Flight.find({ From: fromLocation, To: toLocation });
+            if (flightData.length > 0) {
+                data = flightData;
+            } else {
+                console.log('Flight not found');
+            }
+            return res.render('../views/displayflights.ejs', { data });
+        }
+
+        const query = {
+            From: fromLocation,
+            To: toLocation,
+            DepartDate: new Date(departureDate),
+            ReturnDate: new Date(returnDate),
+            Class: classChoice.toUpperCase() // Ensure class choice matches case
+        };
+
+
+        const flightData = await Flight.findOne(query);
         
-        // Compare the data
         if (flightData) {
-            comparisonResult.from = flightData.From === fromLocation;
-            comparisonResult.to = flightData.To === toLocation;
-            comparisonResult.departDate = new Date(flightData.DepartDate).toISOString().split('T')[0] === departureDate;
-            comparisonResult.returnDate = new Date(flightData.ReturnDate).toISOString().split('T')[0] === returnDate;
-            comparisonResult.class = flightData.Class === classChoice;
+            const comparisonResult = {
+                from: flightData.From === fromLocation,
+                to: flightData.To === toLocation,
+                departDate: new Date(flightData.DepartDate).toISOString().split('T')[0] === departureDate,
+                returnDate: new Date(flightData.ReturnDate).toISOString().split('T')[0] === returnDate,
+                class: flightData.Class === classChoice.toUpperCase()
+            };
+
             if (comparisonResult.from && comparisonResult.to && comparisonResult.departDate && comparisonResult.returnDate && comparisonResult.class) {
                 data.push(flightData);
             }
-        } 
-       else {
-            comparisonResult.error = 'Flight not found';
-        } 
-        res.render('../views/displayflights.ejs', { data});
-    }catch(err){
-        res.status(500)
+        } else {
+            console.log('Flight not found');
+        }
+
+        res.render('../views/displayflights.ejs', { data });
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Internal Server Error');
     }
 });
 
